ea3eaf8b0997d39787436f4c69ae0fd872aa9516
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-11-30 12:57:40 +0530

    Updated bookings migration

--- database/migrations/2022_11_30_051208_create_bookings_table.php
+++ database/migrations/2022_11_30_051208_create_bookings_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->text('purpose_of_visit');
+            $table->string('designation');
+            $table->string('grade_pay');
+            $table->string('employee_id');
+            $table->date('from_date');
+            $table->date('to_date');
+            $table->integer('no_of_persons');
+            $table->foreignId('establishment_id')->constrained('establishments');
+            $table->foreignId('division_id')->constrained('divisions');
             $table->timestamps();
         });
     }
